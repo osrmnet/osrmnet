fd10b283f8680a6c762b1069fb33abce212b487f
Surya Suluh <ssuluh@yahoo.com>
2017-02-19 23:22:49 -0500

    #3 Implement steps=true options. Implement some missing property from route objects and others.

--- src/osrm.net/RouteResult.cpp
+++ src/osrm.net/RouteResult.cpp
@@ -12,35 +12,138 @@
 
 #include <msclr\marshal_cppstd.h>
 
-osrmnet::RouteStep^ ProcessStep(osrm::util::json::Value jsonStep)
+osrmnet::Intersection^ ProcessIntersection(const osrm::util::json::Value jsonIntersection)
 {
-	// TODO: 0.2.x
-	// Implement steps object processing
+	auto result = gcnew osrmnet::Intersection();
+	const auto &intersection = jsonIntersection.get<osrm::json::Object>();
 
-
-	/*const auto &stepObject = jsonStep.get<osrm::json::Object>();
-	const auto distance = stepObject.values.at("distance").get<osrm::json::Number>().value;
-	const auto duration = stepObject.values.at("duration").get<osrm::json::Number>().value;
-	const auto summary = stepObject.values.at("name").get<osrm::json::String>().value;
-	const auto mode = stepObject.values.at("mode").get<osrm::json::String>().value;
-	const auto summary = stepObject.values.at("pronounciation").get<osrm::json::String>().value;
-
-	const auto codedGeometry = stepObject.values.at("geometry").get<osrm::json::String>().value;
-	const auto geometries = osrm::engine::decodePolyline(codedGeometry);
-	for (const osrm::util::Coordinate &coordinate : geometries)
+	// In and Out
+	// in or out can be there or not depending on maneuver type	
+	if(intersection.values.count("out") > 0)
 	{
+		result->Out = System::Convert::ToInt32(intersection.values.at("out").get<osrm::json::Number>().value);
+	}
+	if(intersection.values.count("in") > 0)
+	{
+		result->In = System::Convert::ToInt32(intersection.values.at("in").get<osrm::json::Number>().value);
+
 	}
 
+	// Entry collections
+	const auto& entryList = intersection.values.at("entry").get<osrm::json::Array>().values;
+	for(const auto &entry : entryList)
+	{
+		bool entryTrue = entry.is<osrm::json::True>();
+		result->Entry->Add(entryTrue);
+	}
 
-		const auto &maneuver = stepObject.values.at("maneuver").get<json::Object>().values;
-	const auto type = maneuver.at("type").get<osrm::json::String>().value;*/
+	// Bearings
+	const auto& bearings = intersection.values.at("bearings").get<osrm::json::Array>().values;
+	for (const auto &bearing : bearings)
+	{
+		result->Bearings->Add(System::Convert::ToInt32(bearing.get<osrm::json::Number>().value));
+	}
 
+	// Lanes
+	// Lanes can be optional
+	if (intersection.values.count("lanes") > 0)
+	{
+		const auto& laneList = intersection.values.at("lanes").get<osrm::json::Array>().values;
+		for (const auto &lane : laneList)
+		{
+			result->Lanes->Add(msclr::interop::marshal_as<System::String^>(lane.get<osrm::json::String>().value));
+		}
+	}
 
+	// Location
+	// Caution osrm is long,lat but osrmnet is lat,long just like google
+	const auto &location = intersection.values.at("location").get<osrm::json::Array>().values;
+	result->Location = gcnew osrmnet::Coordinate(location[1].get<osrm::json::Number>().value, location[0].get<osrm::json::Number>().value);
 
-	return gcnew osrmnet::RouteStep();
+	return result;
 }
 
-osrmnet::RouteLeg^ ProcessLeg(const osrm::util::json::Value& jsonLeg, bool generateSteps, bool generateAnnotations)
+osrmnet::Maneuver^ ProcessManeuver(osrm::util::json::Object jsonManeuver)
+{
+	auto result = gcnew osrmnet::Maneuver();
+	result->BearingAfter = System::Convert::ToInt32(jsonManeuver.values.at("bearing_after").get<osrm::json::Number>().value);
+	result->BearingBefore = System::Convert::ToInt32(jsonManeuver.values.at("bearing_before").get<osrm::json::Number>().value);
+	result->Type = msclr::interop::marshal_as<System::String^>(jsonManeuver.values.at("type").get<osrm::json::String>().value);
+	const auto &location = jsonManeuver.values.at("location").get<osrm::json::Array>().values;
+	result->Location = gcnew osrmnet::Coordinate(location[1].get<osrm::json::Number>().value, location[0].get<osrm::json::Number>().value);
+
+	// Optional Modifier property
+	if(jsonManeuver.values.count("modifier"))
+	{
+		result->Modifier = msclr::interop::marshal_as<System::String^>(jsonManeuver.values.at("modifier").get<osrm::json::String>().value);
+	}
+
+	// Optional Exit property
+	if (jsonManeuver.values.count("exit"))
+	{
+		result->Exit = System::Convert::ToInt32(jsonManeuver.values.at("exit").get<osrm::json::Number>().value);
+	}
+
+	return result;
+}
+
+osrmnet::RouteStep^ ProcessStep(osrm::util::json::Value jsonStep)
+{
+	auto routeStep = gcnew osrmnet::RouteStep();
+
+	const auto &stepObject = jsonStep.get<osrm::json::Object>();
+	const auto distance = stepObject.values.at("distance").get<osrm::json::Number>().value;
+	const auto duration = stepObject.values.at("duration").get<osrm::json::Number>().value;
+	const auto name = stepObject.values.at("name").get<osrm::json::String>().value;
+	const auto mode = stepObject.values.at("mode").get<osrm::json::String>().value;
+	const auto geometry = stepObject.values.at("geometry").get<osrm::json::String>().value;
+	
+	routeStep->Geometry = msclr::interop::marshal_as<System::String^>(geometry);
+	routeStep->Mode = msclr::interop::marshal_as<System::String^>(mode);
+	routeStep->Duration = duration;
+	routeStep->Distance = distance;
+	routeStep->Name = msclr::interop::marshal_as<System::String^>(name);
+
+	// Create maneuver
+	routeStep->Maneuver = ProcessManeuver(stepObject.values.at("maneuver").get<osrm::json::Object>());	
+
+	// Create intersections
+	const auto &intersections = stepObject.values.at("intersections").get<osrm::json::Array>().values;
+	for(const auto &intersection : intersections)
+	{
+		routeStep->Intersections->Add(ProcessIntersection(intersection));
+	}
+
+	// Process optional properties
+	if(stepObject.values.count("weight") > 0)
+	{
+		routeStep->Weight = stepObject.values.at("weight").get<osrm::json::Number>().value;
+	}
+
+	if (stepObject.values.count("pronounciation") > 0)
+	{
+		routeStep->Pronounciation = msclr::interop::marshal_as<System::String^>(stepObject.values.at("pronounciation").get<osrm::json::String>().value);
+	}
+
+	if (stepObject.values.count("destinations") > 0)
+	{
+		routeStep->Destinations = msclr::interop::marshal_as<System::String^>(stepObject.values.at("destinations").get<osrm::json::String>().value);
+	}
+
+	if (stepObject.values.count("rotary_name") > 0)
+	{
+		routeStep->RotaryName = msclr::interop::marshal_as<System::String^>(stepObject.values.at("rotary_name").get<osrm::json::String>().value);
+	}
+
+	if (stepObject.values.count("rotary_pronounciation") > 0)
+	{
+		routeStep->RotaryName = msclr::interop::marshal_as<System::String^>(stepObject.values.at("rotary_pronounciation").get<osrm::json::String>().value);
+	}
+
+	return routeStep;
+}
+
+osrmnet::RouteLeg^ ProcessRouteLeg(const osrm::util::json::Value& jsonLeg, bool generateSteps, bool generateAnnotations)
 {
 	const auto &legObject = jsonLeg.get<osrm::json::Object>();
 	const auto distance = legObject.values.at("distance").get<osrm::json::Number>().value;
@@ -67,16 +170,6 @@ osrmnet::RouteLeg^ ProcessLeg(const osrm::util::json::Value& jsonLeg, bool gener
 		const auto &annonDistances = annotationObj.values.at("distance").get<osrm::json::Array>().values;
 		const auto &annonDuration = annotationObj.values.at("duration").get<osrm::json::Array>().values;
 		const auto &annonNodes = annotationObj.values.at("nodes").get<osrm::json::Array>().values;
-		/* Sample Annotation json object.
-		nodes include the start and the end of user supplied so it's always n+1 size
-		{
-			"distance": [5,5,10,5,5],
-			"duration": [15,15,40,15,15],
-			"datasources": [1,0,0,0,1],
-			"nodes": [49772551,49772552,49786799,49786800,49786801,49786802]
-		}
-		*/
-
 		auto annotation = gcnew osrmnet::Annotation();
 		for (auto i = 0; i < annonDistances.size(); i++)
 		{
@@ -102,11 +195,9 @@ System::Collections::Generic::IEnumerable<osrmnet::RouteResult^>^ osrmnet::Route
 	auto &routes = jsonResult.values["routes"].get<osrm::json::Array>().values;
 	for (const auto& route : routes)
 	{
-		const auto& routeObj = route.get<osrm::json::Object>();
-
-		// Build RouteResult^ from json object
 		RouteResult^ routeResult = gcnew RouteResult();
 
+		const auto& routeObj = route.get<osrm::json::Object>();
 		routeResult->Distance = routeObj.values.at("distance").get<osrm::json::Number>().value;
 		routeResult->Duration = routeObj.values.at("duration").get<osrm::json::Number>().value;
 
@@ -114,7 +205,7 @@ System::Collections::Generic::IEnumerable<osrmnet::RouteResult^>^ osrmnet::Route
 		const auto &osrmLegs = routeObj.values.at("legs").get<osrm::json::Array>().values;
 		for (const auto &leg : osrmLegs)
 		{
-			routeResult->Legs->Add(ProcessLeg(leg, routeParameters->Steps, routeParameters->Annotations));
+			routeResult->Legs->Add(ProcessRouteLeg(leg, routeParameters->Steps, routeParameters->Annotations));
 		}
 
 		result->Add(routeResult);
